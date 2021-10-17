2498623938042cb1eeceb376106ddce1557d8d5c
abstract <milanmitrovic070@gmail.com>
2021-10-17 02:31:23 +0200

    CorrectEndpoint

--- config/webhook.php
+++ config/webhook.php
@@ -3,7 +3,7 @@ return [
 
     'local' => [
         'endpoints' =>[
-            'https://webhook.site/bec98a87-f34d-4587-8a71-59e5d99b9275',
+            'https://en0b3xonvuaver.x.pipedream.net',
 
         ],
         'type' => 'json',
